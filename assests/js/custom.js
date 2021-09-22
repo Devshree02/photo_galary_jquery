3b363acfa2c10b48faf62e1a0a454bf98b938107
Devshree02 <devshreenighojakar0205@gmail.com>
2021-09-22 20:12:47 +0530

    Solved some feedback

--- assests/js/custom.js
+++ assests/js/custom.js
@@ -1,32 +1,44 @@
 $(document).ready(function() {
  
-  function toggleIcon() {
-    $(this).hide();
+  $.fn.toggeleIcon = function() {
     if ($(this).attr("data-click")=="cross") {
       $("#searchIcon").show();
+      $("#cross").hide();
     }
     else {
       $("#crossIcon").show();
+      $("#searchIcon").hide();
     }
   }
 
-  $("#crossIcon, #searchIcon").click(toggleIcon);
+  // $("#crossIcon, #searchIcon").click(toggleIcon);
 
   $("#crossIcon").click(function() {
+    $.fn.toggeleIcon();
     $("#imageContainer").show();
     $("#searchResult").hide();
+    $("#searchInput").val("");
   })
 
   $("#searchIcon").click(function() {
+    $.fn.toggeleIcon();
+    // window.alert($(this).attr("data-click"));
     input=$("#searchInput").val();
-    $("#imageContainer img").each(function() {
-      attribute=$(this).attr("data-image");
-      if (attribute.includes(input)) {
-        let image = $('<img>');
-        image.attr("src", $(this).attr("src"));
-        $("#searchResult").append(image);
+    if(input){
+      $("#imageContainer img").each(function() {
+        attribute=$(this).attr("data-image");
+        if (attribute.includes(input)) {
+          let image = $('<img>');
+          image.attr("src", $(this).attr("src"));
+          $("#searchResult").append(image);
+        }
+      })
+      $("#imageContainer").hide();
+    }
+      else {
+        window.alert("Enter a valid key-word");
+        $("#searchIcon").show();
+        $("#crossIcon").hide();
       }
-    })
-    $("#imageContainer").hide();
   });
 });
